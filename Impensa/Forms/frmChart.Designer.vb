67022d41e7720ceea1c1a53a1d50355c8de35ade
Sachin Wadi <sachinwadi@gmail.com>
2019-04-29 11:20:02 +0530

    print chart

--- Impensa/Forms/frmChart.Designer.vb
+++ Impensa/Forms/frmChart.Designer.vb
@@ -22,28 +22,54 @@ Partial Class frmChart
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend
+        Me.components = New System.ComponentModel.Container()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmChart))
-        Me.Chart_Analysis = New System.Windows.Forms.DataVisualization.Charting.Chart
+        Me.Chart_Analysis = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.ChartContextMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.PrintToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintDocument = New System.Drawing.Printing.PrintDocument()
+        Me.PrintDialog = New System.Windows.Forms.PrintDialog()
         CType(Me.Chart_Analysis, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ChartContextMenu.SuspendLayout()
         Me.SuspendLayout()
         '
         'Chart_Analysis
         '
         Me.Chart_Analysis.Dock = System.Windows.Forms.DockStyle.Fill
-        Legend1.Name = "Legend1"
-        Me.Chart_Analysis.Legends.Add(Legend1)
+        Legend2.Name = "Legend1"
+        Me.Chart_Analysis.Legends.Add(Legend2)
         Me.Chart_Analysis.Location = New System.Drawing.Point(0, 0)
         Me.Chart_Analysis.Name = "Chart_Analysis"
         Me.Chart_Analysis.Size = New System.Drawing.Size(724, 413)
         Me.Chart_Analysis.TabIndex = 0
         Me.Chart_Analysis.Text = "Chart1"
         '
+        'ChartContextMenu
+        '
+        Me.ChartContextMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintToolStripMenuItem})
+        Me.ChartContextMenu.Name = "ContextMenuStrip1"
+        Me.ChartContextMenu.Size = New System.Drawing.Size(100, 26)
+        '
+        'PrintToolStripMenuItem
+        '
+        Me.PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
+        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(99, 22)
+        Me.PrintToolStripMenuItem.Text = "Print"
+        '
+        'PrintDocument
+        '
+        '
+        'PrintDialog
+        '
+        Me.PrintDialog.UseEXDialog = True
+        '
         'frmChart
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(724, 413)
+        Me.ContextMenuStrip = Me.ChartContextMenu
         Me.Controls.Add(Me.Chart_Analysis)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
@@ -53,8 +79,13 @@ Partial Class frmChart
         Me.TopMost = True
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         CType(Me.Chart_Analysis, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ChartContextMenu.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents Chart_Analysis As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents ChartContextMenu As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents PrintToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintDocument As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintDialog As System.Windows.Forms.PrintDialog
 End Class
--- Impensa/Forms/frmChart.vb
+++ Impensa/Forms/frmChart.vb
@@ -1,6 +1,45 @@
 ﻿Imports Impensa.clsLib
+Imports System.Drawing.Printing
+
 Public Class frmChart
+    Dim bitmap As Bitmap
+
     Private Sub frmChart_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Call frmMain.DisplayGraph(Chart_Analysis)
+    End Sub
+
+    Private Sub PrintToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PrintToolStripMenuItem.Click
+        Me.SendToBack()
+
+        Dim paperSizes As IEnumerable(Of PaperSize) = PrintDocument.PrinterSettings.PaperSizes.Cast(Of PaperSize)()
+        Dim sizeA4 As PaperSize = paperSizes.First(Function(size) size.Kind = PaperKind.A4)
+        PrintDocument.DefaultPageSettings.PaperSize = sizeA4
+        PrintDocument.DefaultPageSettings.Landscape = True
+
+        PrintDialog.Document = PrintDocument
+        If (PrintDialog.ShowDialog = Windows.Forms.DialogResult.OK) Then PrintDocument.Print()
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(ByVal sender As System.Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintDocument.PrintPage
+        Dim myImage As New Bitmap(Me.Chart_Analysis.Width, Me.Chart_Analysis.Height)
+        Dim PrintSize As Size = e.MarginBounds.Size
+        Dim scale As Double = 1
+        Me.Chart_Analysis.DrawToBitmap(myImage, New Rectangle(Point.Empty, Me.Chart_Analysis.Size))
+
+        If myImage.Width > PrintSize.Width Then
+            'Form is to big. Scale it down.
+            scale = PrintSize.Width / myImage.Width
+            e.Graphics.ScaleTransform(scale, scale)
+        End If
+
+        If (myImage.Height * scale) > PrintSize.Height Then
+            'The form is still to big. Scale it again.
+            scale = PrintSize.Height / (myImage.Height * scale)
+            e.Graphics.ScaleTransform(scale, scale)
+        End If
+
+        e.Graphics.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        e.Graphics.DrawImage(myImage, e.MarginBounds.Location)
+        myImage.Dispose()
     End Sub
 End Class
